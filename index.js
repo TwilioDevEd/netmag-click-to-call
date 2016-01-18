eeb7c9df8ca190ae67733ebbdd4d8df8162278b0
Phil Nash <philnash@gmail.com>
2016-01-18 22:52:47 +0000

    Implements click to call backend

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var config      = require('./config');
 
 // Create our application instance
 var app = express();
+// Create a Twilio API client using Account SID and Auth Token
+var twilioClient = twilio(config.twilioAccountSid, config.twilioAuthToken);
 
 // Setup handlebars as the view engine.
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +24,28 @@ app.get('/', function(request, response) {
 });
 
 app.post('/calls', function(request, response) {
-  // Call the Twilio API here.
-  response.render('thanks');
+  twilioClient.makeCall({
+    to: request.body.phoneNumber,
+    from: config.twilioPhoneNumber,
+    url: 'http://'+config.ngrokUrl+'/calls/forward'
+  }, function(err, responseData){
+    if (err) {
+      response.render('index', {
+        error: err.message
+      });
+    } else {
+      response.render('thanks');
+    }
+  });
 });
 
 app.post('/calls/forward', function(request, response) {
-  // Respond to Twilio with TwiML
-  response.send('');
+  // TwimlResponse is a builder for the XML we need to return.
+  var twiml = new twilio.TwimlResponse();
+  twiml.play('http://bit.ly/netmagmp3');
+
+  response.set('Content-Type', 'text/xml');
+  response.send(twiml.toString());
 });
 
 // Start the app listening on the defined port (or 3000 by default)
